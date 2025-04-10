803792886d29f1c27d49f347665561e222ebdbe3
Antonio Farinetti <afarinetti@users.noreply.github.com>
2025-04-09 20:52:03 -0500

    Continuing example

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,50 @@
 use dioxus::prelude::*;
+use dioxus::logger;
+use dioxus::logger::tracing;
+use gloo::console::log;
+use gloo::utils::document_element;
+use gloo::storage::{LocalStorage, Storage};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+const CSS: Asset = asset!("/assets/tailwind.css");
+const DAISY_CSS: &str = "https://cdn.jsdelivr.net/npm/daisyui@5";
+const DAISY_THEMES_CSS: &str = "https://cdn.jsdelivr.net/npm/daisyui@5/themes.css";
 
 fn main() {
+    // Init logger
+    logger::init(tracing::Level::DEBUG).expect("failed to init logger");
+
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Hero {}
-
+    if let Some(theme) = document_element().get_attribute("data-theme") {
+        tracing::info!("theme = {}", theme);
     }
-}
-
-#[component]
-pub fn Hero() -> Element {
     rsx! {
+        document::Stylesheet { href: CSS },
+        document::Stylesheet { href: DAISY_CSS },
+        document::Stylesheet { href: DAISY_THEMES_CSS },
+        document::Link { href: FAVICON, rel: "icon" }
+
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            div { "Hello, world!" },
+            button {
+                onclick: move |_| handle_click(),
+                class: "btn btn-primary",
+                "Click me!",
             }
         }
     }
+}
+
+fn handle_click() {
+    log!("clicked");
+    
+    let value: String = LocalStorage::get("theme").unwrap_or("light".to_string());
+    // log!("value = %s", value);
+    tracing::debug!("value = {}", value);
+
 }
